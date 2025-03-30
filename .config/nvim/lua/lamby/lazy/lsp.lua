c15a5e8d19dd1c72107157b70b99446a7f70a8e2
John Lamb <lamb.john.n@gmail.com>
2025-03-30 18:20:11 +0200

    Update for new neovim 0.11

--- .config/nvim/lua/lamby/lazy/lsp.lua
+++ .config/nvim/lua/lamby/lazy/lsp.lua
@@ -12,56 +12,13 @@ return {
         require("mason").setup()
         require("mason-lspconfig").setup({
             ensure_installed = {
-                "ruff",
-                "pyright",
                 "lua_ls",
-                "zls",
-                "ols",
-                "ocamllsp",
                 "clangd"
             },
             handlers = {
                 function(server_name) -- default handler (optional)
                     require("lspconfig")[server_name].setup {
                     }
-                end,
-
-                zls = function()
-                    local lspconfig = require("lspconfig")
-                    lspconfig.zls.setup({
-                        root_dir = lspconfig.util.root_pattern(".git", "build.zig", "zls.json"),
-                        settings = {
-                            zls = {
-                                enable_inlay_hints = true,
-                                enable_snippets = true,
-                                warn_style = true,
-                            },
-                        },
-                    })
-                    vim.g.zig_fmt_parse_errors = 0
-                    vim.g.zig_fmt_autosave = 0
-                end,
-
-                ["ols"] = function()
-                    local lspconfig = require("lspconfig")
-                    lspconfig.ols.setup({
-                        init_options = {
-                            checker_args = "-vet -strict-style"
-                        },
-                        filetypes = {'odin'},
-                        root_dir = lspconfig.util.root_pattern(".git", "ols.json", "*.odin"),
-                    })
-                end,
-
-                ["ruff"] = function()
-                    local lspconfig = require("lspconfig")
-                    local on_attach = function(client, bufnr)
-                        client.server_capabilities.hoverProvider = false
-                    end
-                    lspconfig.ruff.setup({
-                        capabilities = capabilities,
-                        on_attach = on_attach,
-                    })
                 end,
 
                 ["pyright"] = function()
@@ -96,11 +53,6 @@ return {
                     }
                 end,
 
-                ["ocamllsp"] = function()
-                    local lspconfig = require("lspconfig")
-                    lspconfig.ocamllsp.setup {
-                    }
-                end,
                 ["clangd"] = function()
                     local lspconfig = require("lspconfig")
                     lspconfig.clangd.setup {
